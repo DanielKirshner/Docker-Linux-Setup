1df34acd5d47e68e4450e4c5a96956c7a7e7fba2
DanielKirshner <danielkirshner7878@gmail.com>
2022-12-20 18:53:02 +0200

    Installing docker and make it run rootless

--- setupDocker.sh
+++ setupDocker.sh
@@ -11,3 +11,9 @@ if [ "$EUID" -ne 0 ]
   then echo -e "\t\tPlease run as root"
   exit 1
 fi
+
+apt-get update
+apt-get install docker.io
+systemctl enable docker.service
+systemctl restart docker.service
+usermod -aG docker $USER
