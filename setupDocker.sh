1e426ebdabce2ff76b29cd2408e1ffb9ad3db3f5
DanielKirshner <danielkirshner7878@gmail.com>
2022-12-23 17:54:56 +0200

    Added reboot logic

--- setupDocker.sh
+++ setupDocker.sh
@@ -19,3 +19,11 @@ echo -e "{\n\t"storage-driver":"vfs"\n}\n" > /etc/docker/daemon.json
 systemctl -q enable docker.service
 systemctl -q restart docker.service
 usermod -aG docker $USER
+
+read -p "Reboot now? [Y/n]" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+        printf "Rebooting...\n"
+        sleep 1
+        reboot
+fi
