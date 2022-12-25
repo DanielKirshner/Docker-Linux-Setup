da972aa72d2fda211c216f34ee53ecc670e2b83b
DanielKirshner <danielkirshner7878@gmail.com>
2022-12-25 16:07:46 +0200

    Revert "Added -y flag to docker installation"
    This reverts commit 5bdeaaa681d0c6e0a52e35779187fc0558d58f6c.

--- setupDocker.sh
+++ setupDocker.sh
@@ -13,7 +13,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 apt-get update
-apt-get install docker.io -y
+apt-get install docker.io
 touch /etc/docker/daemon.json
 echo -e "{\n\t"storage-driver":"vfs"\n}\n" > /etc/docker/daemon.json
 systemctl -q enable docker.service
